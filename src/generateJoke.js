1c6f858d10bb345e412d8d0ffa7c056814ce5a95
stephenkinyua <kinyuamunene.km@gmail.com>
2022-08-29 11:53:57 +0300

    setup webpack config

--- src/generateJoke.js
+++ src/generateJoke.js
@@ -1,5 +1,15 @@
+import axios from "axios";
+
 function generateJoke() {
-  return "foo";
+  const options = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  axios.get("https://icanhazdadjoke.com", options).then((res) => {
+    document.getElementById("joke").innerHTML = res.data.joke;
+  });
 }
 
 export default generateJoke;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import generateJoke from "./generateJoke";
+import "./styles/main.scss";
+import foxImage from "./assets/fox_face.jpg";
 
 console.log(generateJoke());
+
+const foxImgTag = document.getElementById("fox");
+foxImgTag.src = foxImage;
+
+const genJokeBtn = document.getElementById("jokeBtn");
+genJokeBtn.addEventListener("click", generateJoke);
